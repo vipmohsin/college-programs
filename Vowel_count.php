26e4084d07f1dc499955bef4ba23968680801d1b
vipmohsin <mkmohsin404@gmail.com>
2025-04-16 17:18:51 +0500

    feat:update vowel count

--- Vowel_count.php
+++ Vowel_count.php
@@ -5,7 +5,15 @@
 <?php
 include("./header.php");
 
-$name = "";
+$name = "mohsin khan";
+$len = strlen($name);
+for($i = 0; $i < $len ; $i++){
+    $sub = substr($name,$i,1);
+        
+    if($sub == "a" || $sub == "o" || $sub == "i" || $sub == "o" || $sub == "u" ){
+        $vowel_count++;
+    }
+}
 
 
 ?>
