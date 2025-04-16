76f21d4de353ba855d40faf4a1c779a8b757d966
vipmohsin <mkmohsin404@gmail.com>
2025-04-16 17:29:37 +0500

    feat: vowels count program using for loop completed

--- Vowel_count.php
+++ Vowel_count.php
@@ -5,15 +5,19 @@
 <?php
 include("./header.php");
 
-$name = "mohsin khan";
+$name = "mohsin faheem mehtab khan";
+$name = strtolower($name);
 $len = strlen($name);
+$vowel_count=0;
+$vowels="";
 for($i = 0; $i < $len ; $i++){
     $sub = substr($name,$i,1);
         
-    if($sub == "a" || $sub == "o" || $sub == "i" || $sub == "o" || $sub == "u" ){
+    if($sub == "a" || $sub == "e" || $sub == "i" || $sub == "o" || $sub == "u" ){
         $vowel_count++;
+        $vowels.=$sub;
     }
 }
-
+echo "Input : <b>$name</b> <br> The Number of vowels in Given name are <b>$vowel_count</b> <br> They are <b>$vowels";
 
 ?>
--- header.php
+++ header.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>College Programs || By Mohsin Khan</title>
     <link rel="icon" href="./images/icon.jpg">
+    <style>
+        * {
+            margin: 30;
+            padding: 0;
+        }
+
+        body {
+         font: 1em sans-serif;
+         
+        }
+    </style>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
